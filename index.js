1e6bd43467f1d977145c8de13754af8d316bad00
MohammadFaysal731 <www.faysal731@gmail.com>
2023-04-16 10:40:16 +0600

    create delete api for service

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
       const services = await servicesCollection.insertOne(serviceData);
       res.send(services);
     });
+    //this api for delete a service
+    app.delete('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query ={_id: new ObjectId(id)};
+      const result = await servicesCollection.deleteOne(query);
+      res.send(result);
+    })
     //  this api for get all bookings
     app.get("/bookings",verifyJWT, async (req, res) => {
       const bookings = await bookingCollection.find().toArray();
@@ -159,10 +166,10 @@ async function run() {
       
     });
     //this api for delete user
-    app.delete("/user/:id",async(req,res)=>{
-      const id =req.params.id;
-      const query = {_id:new ObjectId(id)}
-      const result = await userCollection.deleteOne(query);
+    app.delete("/user/:email",async(req,res)=>{
+      const email = req.params.email;
+      const filter = {email:email}
+      const result = await userCollection.deleteOne(filter);
       res.send(result)
     })
   } 
